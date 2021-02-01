3fe3b65d9c28e814e3039a30603100fc1815f58a
Gunnar Inge G. Sortland <gi@wgtwo.com>
2021-02-01 16:06:56 +0100

    cleanup

--- leaf-python-mqtt.py
+++ leaf-python-mqtt.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 
+import json
+import logging
+import os
+import paho.mqtt.client as mqtt
+import pprint
 import pycarwings2
+import schedule
+import sys
 import time
 from configparser import SafeConfigParser
-import logging
-import sys
-import pprint
-from environs import Env, EnvError
-import paho.mqtt.client as mqtt
-import schedule
 from datetime import datetime, timedelta
 from decimal import Decimal
-import os
-import json
+from environs import Env, EnvError
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logging.info("Startup leaf-python-MQTT: " +
-             datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='%(asctime)-15s %(levelname)s %(message)s')
+logging.info("Starting...")
 
 
-def duration(duration_string):  # example: '5d3h2m1s'
+def duration(duration_string):
     duration_string = duration_string.lower()
     total_seconds = Decimal('0')
     prev_num = []
@@ -62,22 +64,23 @@ except EnvError as e:
 
 logging.info("updating data from API every %s" % str(GET_UPDATE_INTERVAL))
 
-# The callback for when the client receives a CONNACK response from the server.
+
 def on_connect(client, userdata, flags, rc):
-    logging.info("Connected to MQTT host " +
-                 mqtt_host + " with result code "+str(rc))
-    logging.info("Suscribing to leaf control topic: " + mqtt_control_topic)
+    logging.info(
+        "Connected to MQTT host %s with result code %d", mqtt_host, rc)
+    logging.info("Subscribing to topic: %s/#", mqtt_control_topic)
     client.subscribe(mqtt_control_topic + "/#")
-    logging.info("Publishing to leaf status topic: " + mqtt_status_topic)
+    logging.info("Publishing to leaf status topic: %s", mqtt_status_topic)
     client.publish(mqtt_status_topic, "MQTT connected")
 
-# The callback for when a PUBLISH message is received from the server.
+
 def on_message(client, userdata, msg):
     control_subtopic = msg.topic.rsplit('/', 1)[1]
     control_message = msg.payload.decode().strip(' "').lower()
-    logging.info("Received message topic=%s message=%s" % (msg.topic, control_message))
+    logging.info("Received message topic=%s message=%s",
+                 msg.topic, control_message)
 
-    # If climate control messaage is received mqtt_control_topic/climate
+    # Handle messages for {mqtt_control_topic}/climate
     if control_subtopic == 'climate':
         logging.info('Climate control command received: ' + control_message)
 
@@ -86,14 +89,16 @@ def on_message(client, userdata, msg):
         elif control_message == "off":
             climate_control(0)
         else:
-          logging.warn("Invalid payload for topic=%s: %s" % msg.topic, control_message)
+            logging.warn("Invalid payload for topic=%s: %s",
+                         msg.topic, control_message)
 
-    # If climate control messaage is received on mqtt_control_topic/update
+    # Handle messages for {mqtt_control_topic}/update
     if control_subtopic == 'update':
         logging.info('Update control command received: ' + control_message)
         leaf_info = get_leaf_update()
         time.sleep(10)
         mqtt_publish(leaf_info)
+
 
 client = mqtt.Client()
 # Callback when MQTT is connected
@@ -112,55 +117,54 @@ client.loop_start()
 
 
 def climate_control(climate_control_instruction):
-    logging.debug("login = %s , password = %s" % (username, password))
     logging.info("Prepare Session climate control update")
     s = pycarwings2.Session(username, password, nissan_region_code)
     logging.info("Login...")
-    logging.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     l = s.get_leaf()
 
     if climate_control_instruction == 1:
-        logging.info("Turning on climate control..wait 60s")
+        logging.info("Turning on climate control...")
         try:
-          result_key = l.start_climate_control()
-          time.sleep(60)
-          start_cc_result = l.get_start_climate_control_result(result_key)
-          logging.info(start_cc_result)
+            result_key = l.start_climate_control()
+            logging.info("Waiting 60 seconds...")
+            time.sleep(60)
+            start_cc_result = l.get_start_climate_control_result(result_key)
+            logging.info(start_cc_result)
         except:
-          logging.warn("Error while starting climate control")
+            logging.warn("Error while starting climate control")
 
     if climate_control_instruction == 0:
-        logging.info("Turning off climate control..wait 60s")
+        logging.info("Turning off climate control...")
         try:
-          result_key = l.stop_climate_control()
-          time.sleep(60)
-          stop_cc_result = l.get_stop_climate_control_result(result_key)
-          logging.info(stop_cc_result)
+            result_key = l.stop_climate_control()
+            logging.info("Waiting 60 seconds...")
+            time.sleep(60)
+            stop_cc_result = l.get_stop_climate_control_result(result_key)
+            logging.info(stop_cc_result)
         except:
-          logging.warn("Error while stopping climate control")
+            logging.warn("Error while stopping climate control")
 
 
 # Request update from car, use carefully: requires car GSM modem to powerup
 def get_leaf_update():
-    logging.debug("login = %s , password = %s" % (username, password))
-    logging.info("Prepare Session get car update")
+    logging.info("Preparing session to get car update")
     s = pycarwings2.Session(username, password, nissan_region_code)
     logging.info("Login...")
-    logging.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     try:
         l = s.get_leaf()
     except:
         logging.error("CarWings API error")
-    logging.info("Requesting update from car..wait 30s")
+    logging.info("Requesting update from car...")
     try:
         result_key = l.request_update()
     except:
-        logging.error("ERROR: no responce from car update")
+        logging.error("No response from car update")
+    logging.info("Waiting 30 seconds...")
     time.sleep(30)
     battery_status = l.get_status_from_update(result_key)
 
     while battery_status is None:
-        logging.error("ERROR: no responce from car")
+        logging.error("No response from car")
         time.sleep(10)
         battery_status = l.get_status_from_update(result_key)
 
@@ -171,12 +175,9 @@ def get_leaf_update():
 
 
 def get_leaf_status():
-    logging.debug("login = %s , password = %s" % (username, password))
-    logging.info("Prepare Session")
+    logging.info("Preparing session")
     s = pycarwings2.Session(username, password, nissan_region_code)
     logging.info("Login...")
-    logging.info("Start update time: " +
-                 datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
     try:
         l = s.get_leaf()
@@ -185,7 +186,6 @@ def get_leaf_status():
         return
 
     logging.info("get_latest_battery_status")
-
     leaf_info = l.get_latest_battery_status()
 
     if leaf_info:
@@ -214,15 +214,9 @@ def get_leaf_status():
         logging.info("leaf_info.battery_percent %s" %
                      leaf_info.battery_percent)
 
-        # logging.info("getting climate update")
-        # climate = l.get_latest_hvac_status()
-        # pprint.pprint(climate)
-
         mqtt_publish(leaf_info)
 
-        logging.info("End update time: " +
-                     datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        logging.info("Schedule API update every %s" % str(GET_UPDATE_INTERVAL))
+        logging.info("Update finished")
         return (leaf_info)
     else:
         logging.info("Did not get any response from the API")
@@ -230,10 +224,7 @@ def get_leaf_status():
 
 
 def mqtt_publish(leaf_info):
-    logging.info("End update time: " +
-                 datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-    logging.info("publishing to MQTT base status topic: " + mqtt_status_topic)
+    logging.info("Publishing to MQTT base status topic: " + mqtt_status_topic)
     client.publish(mqtt_status_topic + "/last_updated",
                    leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"])
     time.sleep(1)
